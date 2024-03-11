d2ba28d9759465cb9af22d8ff291e787820f30d5
Camilo Estrada Patiño <automatizador8@ariafina.co>
2024-03-11 03:11:43 -0500

    Iconos y bd

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://192.168.1.5:3000/api"
+const BASE_URL = "https://backend-motorbikefull.onrender.com/api"
 
 const axiosClient = axios.create({
   baseURL: BASE_URL,
